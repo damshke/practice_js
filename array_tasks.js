92bec3ceb04b442889f6cc2b29a29c63b0e4ece9
Daria <egorova@greensight.ru>
2023-10-19 12:24:52 +0300

    fixed implementation with map

--- array_tasks.js
+++ array_tasks.js
@@ -1,19 +1,7 @@
 const arr = ['banana', true, 1, 'car', {}, { a: 1 }, 5, true, true, false, 455, {}]
 
 // first
-// вариант с forEach
 
-function multiplOnyTwo(arr) {
-    let newArr = []
-    arr.forEach(element => {
-        Number.isFinite(element) ? newArr.push(element * 2) : newArr.push(element)
-    })
-    return newArr
-}
+const multiplyArray = (arr) => arr.map((item) => (Number.isFinite(item) ? item * 2 : item));
 
-// вариант с map
-
-const multiplyArray = arr.map(item => Number.isFinite(item) ? item * 2 : item)
-
-console.log(multiplyArray)
-console.log(multiplyOnTwo(arr))
+console.log(multiplyArray(arr))
