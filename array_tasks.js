311e07b121b1fc49f3c627ead8481894619f0db4
Daria <egorova@greensight.ru>
2023-10-19 14:02:39 +0300

    #12: fixed howManyDataTypes function

--- array_tasks.js
+++ array_tasks.js
@@ -1,11 +1,9 @@
 // two
 const arr = ['banana', true, 1, 'car', {}, { a: 1 }, 5, true, true, false, 455, {}]
-function howManyDataTypes(arr) {
-    let obj = {}
-    arr.forEach(element => {
-        obj[typeof (element)] == undefined ? obj[typeof (element)] = 1 : obj[typeof (element)] = obj[typeof (element)] + 1;
-    })
-    return obj
-}
+
+const howManyDataTypes = (arr) => arr.reduce((obj, item) => {
+    obj[typeof (item)] = (obj[typeof (item)] || 0) + 1;
+    return obj;
+}, {})
 
 console.log(howManyDataTypes(arr))
