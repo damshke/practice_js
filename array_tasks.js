e1794bab18e90e950812cd555750b5951aa7f407
Daria <egorova@greensight.ru>
2023-10-23 10:17:06 +0300

    #15: removed unnecessary code

--- array_tasks.js
+++ array_tasks.js
@@ -1,13 +1,6 @@
 const wordsArr = ['fruit', 'keyboard', 'word', 'word', 'keyboard', 'word', 'fruit', 'banana']
 
-function sortingWordsArr(arr) {
-    let newMap = new Map()
-    arr.map(item => newMap.set(item, newMap.has(item) ? newMap.get(item) + 1 : 1))
-    const sortedArr = new Map([...newMap.entries()].sort((a, b) => b[1] - a[1]))
-    return [...sortedArr.keys()]
-}
-
-// console.log(sortingWordsArr(wordsArr))
+// fifth
 
 const sorting = (arr) => {
     const dict = arr.reduce((acc, item) => { acc[item] = acc[item] ? acc[item] + 1 : 1; return acc }, {});
