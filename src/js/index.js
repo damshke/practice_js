3402b8f3b2bbc5cdae80a9a63b9d583f4cf78281
Daria <egorova@greensight.ru>
2023-10-24 17:50:53 +0300

    #5: started filling card with data from fetch

--- src/js/index.js
+++ src/js/index.js
@@ -19,7 +19,9 @@ async function fetchFiltersData() {
     return data.employment, data.experience;
 }
 
+function fillingCard() {
 
+}
 
 async function getVacancyData(page, per_page) {
 
@@ -30,56 +32,16 @@ async function getVacancyData(page, per_page) {
     }
 
     data.items.forEach(item => {
-        // output =
-        //     <div class="vacancy-card__head">
-        //         <div class="vacancy-card__info">
-        //             <div class="vacancy-card__left">
-        //                 <h4 class="vacancy-card__title">${item.name}</h4>
-        //                 <img class="vacancy-card__logo" src={item.employer.logo_urls}></img>
-        //             </div>
-        //             <button class="vacancy-card__respond-button button">Respond</button>
-        //         </div>
-        //         <div class="vacancy-card__about">
-        //             <p>
-        //                 <span class="vacancy-card__option">Form</span>
-        //                 <span class="vacancy-card__text">${item.employment.name}</span>
-        //             </p>
-        //             <p>
-        //                 <span class="vacancy-card__option">Company</span>
-        //                 <span class="vacancy-card__text">${item.employer.name}</span>
-        //             </p>
-        //             <p>
-        //                 <span class="vacancy-card__option">Experience</span>
-        //                 <span class="vacancy-card__text">00 years</span>
-        //             </p>
-        //             <p>
-        //                 <span class="vacancy-card__option">Address</span>
-        //                 <span class="vacancy-card__text">Address</span>
-        //             </p>
-        //             <p>
-        //                 <span class="vacancy-card__option">Salary</span>
-        //                 <span class="vacancy-card__text">from to</span>
-        //             </p>
-        //         </div>
-        //         <div class="vacancy-card__description">
-        //             <div class="vacancy-card__description-content">
-        //                 Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
-        //                 totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
-        //                 dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
-        //                 sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
-        //                 est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius
-        //                 modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima
-        //                 veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea
-        //                 commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil
-        //                 molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+        const vacancyName = document.querySelector(".vacancy-card__title");
+        const vacancyImg = document.querySelector(".vacancy-card__logo");
+        const vacancyForm = document.querySelector(".vacancy-card__text");
+        vacancyName.innerHTML = item.name;
+        vacancyImg.src = item.employer.logo_urls;
+        vacancyForm.innerHTML = item.employment.name;
 
-        //             </div>
-        //             <button class="vacancy-card__expand-button">More details</button>
-        //         </div>
-        //     </div>
 
-        // document.querySelector('.vacancy-card').innerHTML += output;
     });
+
 
 
 }
