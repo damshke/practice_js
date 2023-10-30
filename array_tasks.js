35adfe5a697206cf6ab3786a05564e32e16a4bf0
Daria <egorova@greensight.ru>
2023-10-30 10:33:03 +0300

    #3: removed unnecessary returns

--- array_tasks.js
+++ array_tasks.js
@@ -1,4 +1,5 @@
 const wordsArr = ['fruit', 'keyboard', 'word', 'word', 'keyboard', 'word', 'fruit', 'banana']
+const arr = ['banana', true, 1, 'car', {}, { a: 1 }, 5, true, true, false, 455, {}]
 
 // first
 
@@ -7,8 +8,6 @@ const multiplyArray = (arr) => arr.map((item) => (Number.isFinite(item) ? item *
 console.log(multiplyArray(arr))
 
 // two
-
-const arr = ['banana', true, 1, 'car', {}, { a: 1 }, 5, true, true, false, 455, {}]
 
 const howManyDataTypes = (arr) => arr.reduce((obj, item) => {
     obj[typeof (item)] = (obj[typeof (item)] || 0) + 1;
@@ -21,9 +20,7 @@ console.log(howManyDataTypes(arr))
 
 function specialSorting(arr) {
     const dataTypes = ['boolean', 'number', 'string', 'object']
-    return dataTypes.reduce((newArr, item) => {
-        return newArr.concat(arr.filter(elem => { return typeof (elem) === item }))
-    }, [])
+    return dataTypes.reduce((newArr, item) => newArr.concat(arr.filter(elem => typeof (elem) === item)), [])
 }
 
 const newArrSort = specialSorting(arr)
@@ -39,7 +36,7 @@ console.log(newArr(3, 4, 5, 6))
 // fifth
 
 const sorting = (arr) => {
-    const dict = arr.reduce((acc, item) => { acc[item] = acc[item] ? acc[item] + 1 : 1; return acc }, {});
+    const dict = arr.reduce((acc, item) => { acc[item] = acc[item] + 1 || 1; return acc }, {});
     return Object.keys(dict).sort((a, b) => dict[b] !== dict[a] ? dict[b] - dict[a] : a.localeCompare(b))
 }
 
