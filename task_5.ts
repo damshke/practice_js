906ff1ad81adac0398b2519d7ce8d98a8bc36605
Daria <egorova@greensight.ru>
2023-11-13 11:23:57 +0300

    #95: changed unknown to AllType

--- task_5.ts
+++ task_5.ts
@@ -1,7 +1,15 @@
 // написать пример типизации метода map у массивов
 
-function newMap(fn: (a: unknown) => unknown, arr: unknown[]) {
-    let new_arr: Array<unknown> = []
+type AllTypes =
+    | boolean
+    | number
+    | string
+    | null
+    | undefined
+    | object;
+
+function newMap(fn: (a: AllTypes) => AllTypes, arr: AllTypes[]) {
+    let new_arr: Array<AllTypes> = []
     arr.forEach((element) => new_arr.push(fn(element)));
     return new_arr;
 
