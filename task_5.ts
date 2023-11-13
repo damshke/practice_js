729d43c7130479a280caaa9f44cc6931d3f7272f
Daria <egorova@greensight.ru>
2023-11-13 11:14:44 +0300

    #95: changed any to unknown

--- task_5.ts
+++ task_5.ts
@@ -1,7 +1,7 @@
 // написать пример типизации метода map у массивов
 
-function newMap<T>(fn: (a: T) => T | any, arr: T[] | any[]) {
-    let new_arr: Array<T> = []
+function newMap(fn: (a: unknown) => unknown, arr: unknown[]) {
+    let new_arr: Array<unknown> = []
     arr.forEach((element) => new_arr.push(fn(element)));
     return new_arr;
 
@@ -10,8 +10,8 @@ function newMap<T>(fn: (a: T) => T | any, arr: T[] | any[]) {
 const array = [1, 2, 3];
 const array_string = ['a', 'b', 'c'];
 
-const new_array = newMap<number>((x) => x + 'x', array);
-const new_array_string = newMap<string>((x) => x + 'a', array_string);
+const new_array = newMap((x) => x + 'x', array);
+const new_array_string = newMap((x) => x + 'a', array_string);
 
 console.log(new_array);
 console.log(new_array_string);
