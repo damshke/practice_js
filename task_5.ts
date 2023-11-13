16b8f3c79791d3fbd659158cad2bbb3a0aa37821
Daria <egorova@greensight.ru>
2023-11-13 21:27:35 +0300

    #95: added ternary operator

--- task_5.ts
+++ task_5.ts
@@ -16,13 +16,10 @@ const array_string = ["a", "b", "c"];
 
 const new_array = newMap((x, index) => x + index, array);
 const new_array_string = newMap((x) => x + "a", array_string);
-const new_array_with_prev = newMap((x, index, array) => {
-  if (index > 0) {
-    return x - array[index - 1];
-  } else {
-    return x;
-  }
-}, array);
+const new_array_with_prev = newMap(
+  (x, index, array) => (index > 0 ? x - array[index - 1] : x),
+  array
+);
 
 console.log(new_array);
 console.log(new_array_string);
