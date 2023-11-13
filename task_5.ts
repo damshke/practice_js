01492119d08aa0ef431c71f08895d3f34bedbd74
Daria <egorova@greensight.ru>
2023-11-13 21:26:47 +0300

    #95: fixed types for newMap and added variables like in map

--- task_5.ts
+++ task_5.ts
@@ -1,25 +1,29 @@
 // написать пример типизации метода map у массивов
 
-type AllTypes =
-    | boolean
-    | number
-    | string
-    | null
-    | undefined
-    | object;
-
-function newMap(fn: (a: AllTypes) => AllTypes, arr: AllTypes[]) {
-    let new_arr: Array<AllTypes> = []
-    arr.forEach((element) => new_arr.push(fn(element)));
-    return new_arr;
-
+function newMap<Input, Output>(
+  fn: (a: Input, index: number, array: Input[]) => Output,
+  arr: Input[]
+) {
+  let new_arr: Array<ReturnType<typeof fn>> = [];
+  arr.forEach((element, index, array) =>
+    new_arr.push(fn(element, index, array))
+  );
+  return new_arr;
 }
 
 const array = [1, 2, 3];
-const array_string = ['a', 'b', 'c'];
+const array_string = ["a", "b", "c"];
 
-const new_array = newMap((x) => x + 'x', array);
-const new_array_string = newMap((x) => x + 'a', array_string);
+const new_array = newMap((x, index) => x + index, array);
+const new_array_string = newMap((x) => x + "a", array_string);
+const new_array_with_prev = newMap((x, index, array) => {
+  if (index > 0) {
+    return x - array[index - 1];
+  } else {
+    return x;
+  }
+}, array);
 
 console.log(new_array);
 console.log(new_array_string);
+console.log(new_array_with_prev);
