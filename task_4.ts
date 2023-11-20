f12889d338f8f0db65dcc87cdae63fb070dddb1d
Daria <egorova@greensight.ru>
2023-11-20 14:49:20 +0300

    #94: fixed with generics

--- task_4.ts
+++ task_4.ts
@@ -1,25 +1,22 @@
-type Output = {
-    [key: string]: Object[]
+type Output<T> = { [key: string]: T[] };
+
+function groupBy<T>(arr: T[], key: keyof T): Output<T> {
+  return arr.reduce((acc: Output<T>, cur) => {
+    const groupKey = cur[key];
+    if (!acc[groupKey as string]) {
+      acc[groupKey as string] = [];
+    }
+    acc[groupKey as string].push(cur);
+    return acc;
+  }, {});
 }
 
-function groupBy(arr: object[], key: string): Object {
-    return arr.reduce((acc: Output, cur) => {
-        const groupKey = cur[key as keyof typeof cur];        
-        if (!acc[groupKey]) {
-            acc[groupKey] = [];
-        }
-        acc[groupKey].push(cur);
-        return acc;
-    }, {});
-}
-
-const objects: Object[] = [
-    { id: 1, name: 'Alice', age: 25 },
-    { id: 2, name: 'Bob', age: 30 },
-    { id: 3, name: 'Charlie', age: 25 },
-    { id: 4, name: 'David', age: 30 },
+const objects: { id: number; name: string; age: number }[] = [
+  { id: 1, name: "Alice", age: 25 },
+  { id: 2, name: "Bob", age: 30 },
+  { id: 3, name: "Charlie", age: 25 },
+  { id: 4, name: "David", age: 30 },
 ];
 
-const groupedByAge: Object = groupBy(objects, 'age');
+const groupedByAge: Output<(typeof objects)[0]> = groupBy(objects, "age");
 console.log(groupedByAge);
-
