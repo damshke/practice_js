f0d6ca0033f275e0aa2f14ac0e9ce308dfdb0d72
Daria <egorova@greensight.ru>
2023-11-30 10:42:16 +0300

    #9: fixed Main

--- hh_task/src/containers/Main/index.tsx
+++ hh_task/src/containers/Main/index.tsx
@@ -1,31 +1,9 @@
-/* eslint-disable no-nested-ternary */
-import FeedbackForm from '@views/vacancies/FeedbackForm';
-import CardList from '@containers/CardList';
-import Pagination from '@components/Pagination';
-import { useState } from 'react';
-import Filters from '@views/vacancies/Filters';
-import { useVacancies } from '@api/vacancies';
+import React, { ReactNode } from 'react';
 
-export default function Main() {
-    const [page, setPage] = useState(0);
-    const [filters, setFilters] = useState({ schedule: '', employment: '' });
+type MainProps = {
+    children?: ReactNode;
+};
 
-    const { isLoading, isError, error, data } = useVacancies({ page, filters });
+const Main: React.FC<MainProps> = ({ children }) => <main>{children}</main>;
 
-    return (
-        <main>
-            <Filters onSubmit={setFilters} />
-            {isLoading ? (
-                <span>Loading...</span>
-            ) : isError ? (
-                <span>Error: {error.message}</span>
-            ) : (
-                <div>
-                    <CardList vacancies={data?.items} />
-                    <Pagination setPage={setPage} page={page} totalPages={data?.pages} />
-                </div>
-            )}
-            <FeedbackForm />
-        </main>
-    );
-}
+export default Main;
--- hh_task/src/views/index.tsx
+++ hh_task/src/views/index.tsx
@@ -1,13 +1,36 @@
 import Main from '@containers/Main';
+import { useState } from 'react';
+import { useVacancies } from '@api/vacancies';
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 import { VACANCIES_KEY, API_URL } from '@scripts/const';
 import { getMethod } from '@api/index';
+import CardList from '@containers/CardList';
+import Pagination from '@components/Pagination';
+import Filters from './vacancies/Filters';
+import FeedbackForm from './vacancies/FeedbackForm';
 
 // @ts-ignore next-line
 export default function Home({ dehydrateState }) {
+    const [page, setPage] = useState(0);
+    const [filters, setFilters] = useState({ schedule: '', employment: '' });
+
+    const { isLoading, isError, error, data } = useVacancies({ page, filters });
     return (
         <HydrationBoundary state={dehydrateState}>
-            <Main />
+            <Main>
+                <Filters onSubmit={setFilters} />
+                {isLoading ? (
+                    <span>Loading...</span>
+                ) : isError ? (
+                    <span>Error: {error.message}</span>
+                ) : (
+                    <div>
+                        <CardList vacancies={data?.items} />
+                        <Pagination setPage={setPage} page={page} totalPages={data?.pages} />
+                    </div>
+                )}
+                <FeedbackForm />
+            </Main>
         </HydrationBoundary>
     );
 }
